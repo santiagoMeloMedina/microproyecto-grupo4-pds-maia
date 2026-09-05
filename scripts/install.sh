9fa4922b87332f1db5e12e54ff46d17f1a663cd1
EdissonPrieto-es <ed.prieto@uniandes.edu.co>
2026-09-05 10:47:52 -0500

    ubuntu script added

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,12 @@ set -euo pipefail
 
 VENV_DIR=".venv"
 
+if command -v apt-get >/dev/null 2>&1; then
+	sudo apt update
+	sudo apt install -y make python3-pip python3-venv
+	sudo apt-get install -y libgomp1
+fi
+
 if [ ! -d "$VENV_DIR" ]; then
 	python3 -m venv "$VENV_DIR"
 fi
@@ -12,3 +18,4 @@ source "$VENV_DIR/bin/activate"
 pip install --upgrade pip
 pip install "dvc[s3]"
 pip install -r exploration/requirements.txt
+pip install -r models/requirements.txt
